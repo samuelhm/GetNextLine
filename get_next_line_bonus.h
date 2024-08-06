8000d28e7503768de3f34e8209929a52b80289ef
Samuel Hurtado Marin <shurtado@car7s4.42barcelona.com>
2024-08-06 16:33:38 +0200

    cloned files to start bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shurtado <shurtado@student.42barcelona.fr  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/03 04:55:12 by shurtado          #+#    #+#             */
+/*   Updated: 2024/08/06 16:30:43 by shurtado         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 512
+# endif
+
+# include <stdlib.h>
+# include <stddef.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+int		ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, int len);
+char	*ft_strchr(const char *s, int c);
+
+#endif
