6bfbe3df132f42b59d9de8d1bccd359b832c6357
Samuel Hurtado Marin <shurtado@car5s2.42barcelona.com>
2024-08-08 11:40:41 +0200

    Header fix

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,12 +6,12 @@
 /*   By: shurtado <shurtado@student.42barcelona.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 04:55:12 by shurtado          #+#    #+#             */
-/*   Updated: 2024/08/08 08:09:36 by shurtado         ###   ########.fr       */
+/*   Updated: 2024/08/08 11:37:53 by shurtado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 512
